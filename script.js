e40d749c2787b458390bf889fff41343cd5c7daa
Abdul Munam <abdulmunam791@gmail.com>
2025-09-26 04:43:15 +0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+let unitbtn = document.querySelector(".dropdown-btn");
+let dropdown = document.querySelector(".dropdown");
+
+unitbtn.addEventListener("click", () => {
+    dropdown.classList.toggle("dropdowndisplay")
+});
+
+
+async function fetchwheather(location) {
+    try{
+       let geors = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${location}`)
+       let geodata = await geors.json();
+
+       if(!geodata.results || geodata.results.lenght === 0 ){
+            console.log("Enter a valid location")
+            return
+       }
+
+       let { latitude, longitude, name, country }  = geodata.results[0];
+
+       
+
+        
+
+
+
+
+    }
+    catch(err){
+
+    }
+}
